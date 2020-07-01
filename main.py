6f85ce85d9044a53453cbc1d2373685262d1d54a
ukishere <61353722+ukishere@users.noreply.github.com>
2020-07-01 11:14:21 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 class Stack:
 
     def __init__(self):
-        self.stack = ''
+        self.stack = []
 
     def isEmpty(self):
         if len(self.stack) == 0:
@@ -10,12 +10,10 @@ class Stack:
             return False
 
     def push(self, new_element):
-        self.stack = self.stack + str(new_element)
+        self.stack.append(new_element)
 
     def pop(self):
-        last_element = self.stack[len(self.stack)-1]
-        self.stack = self.stack[0:len(self.stack)-1]
-        return last_element
+        self.stack.pop()
 
     def peek(self):
         return self.stack[len(self.stack)-1]
@@ -25,36 +23,21 @@ class Stack:
 
 def check_stack(test_string):
 
-    stack.stack = ''
+    stack.stack = []
     for string in test_string:
-        stack.push(string)
+        if stack.size() == 0:
+            stack.push(string)
+        else:
+            if stack.peek() == '(' and string  == ')':
+                stack.pop()
+            elif stack.peek() == '[' and string  == ']':
+                stack.pop()
+            elif stack.peek() == '{' and string == '}':
+                stack.pop()
+            else:
+                stack.push(string)
 
-    if stack.size()%2 != 0:
-        return 'несбалансирован'
-
-    type1 = 0
-    type2 = 0
-    type3 = 0
-
-    while stack.isEmpty() != True:
-        test_item = stack.pop()
-        if test_item == '(': type1 += 1
-        elif test_item == ')':
-            type1 -= 1
-            if stack.peek() == '[' or stack.peek() == '{':
-                return 'несбалансирован'
-        elif test_item == '[': type2 += 1
-        elif test_item == ']':
-            type2 -= 1
-            if stack.peek() == '(' or stack.peek() == '{':
-                return 'несбалансирован'
-        elif test_item == '{': type3 += 1
-        elif test_item == '}':
-            type3 -= 1
-            if stack.peek() == '[' or stack.peek() == '(':
-                return 'несбалансирован'
-
-    if not type1 and not type2 and not type3:
+    if stack.isEmpty():
         return 'сбалансирован'
     else:
         return 'несбалансирован'
